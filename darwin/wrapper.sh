3f5a13d677b58900cf81dac8518ffa90de3310af
andre4ik3 <andre4ik3@fastmail.com>
2025-08-16 02:53:40 +0000

    Fix (Neo)Forge installers

--- darwin/wrapper.sh
+++ darwin/wrapper.sh
@@ -29,9 +29,6 @@ getenv() {
   esac
 }
 
-# Process config from environment variables
-allow_microphone="$(getenv "SANDBOX_ALLOW_MICROPHONE" "false")"
-allow_multiplayer="$(getenv "SANDBOX_ALLOW_MULTIPLAYER" "true")"
 
 # Find classpath and natives directory, process arguments
 classpath="0"
@@ -55,9 +52,18 @@ done
 # Ensure the working directory is the instance folder
 cd "$INST_MC_DIR" || exit 1
 
+# Are we running (Neo)Forge?
+echo "$classpath" | grep -q "forge" && is_forge="true" || is_forge="false"
+
+# Process config from environment variables
+allow_all_libraries="$(getenv "SANDBOX_ALLOW_ALL_LIBRARIES" "$is_forge")"
+allow_microphone="$(getenv "SANDBOX_ALLOW_MICROPHONE" "false")"
+allow_multiplayer="$(getenv "SANDBOX_ALLOW_MULTIPLAYER" "true")"
+
 # Other directories
 minecraft="$(realpath "$INST_MC_DIR")"
 assets="$(realpath "$INST_DIR/../../assets")"
+libraries="$(realpath "$INST_DIR/../../libraries")"
 java="$(realpath "$INST_JAVA")"
 java_home="$(realpath "$(echo "$java" | sed "s;/Contents/Home/.*;;")"/..)"
 
@@ -67,8 +73,10 @@ echo "Executing Minecraft with macOS Sandbox Wrapper"
 echo
 echo " => Minecraft: $(echo "$minecraft" | sed "s;$HOME;~;")"
 echo " => Assets: $(echo "$assets" | sed "s;$HOME;~;")"
+echo " => Libraries: $(echo "$libraries" | sed "s;$HOME;~;")"
 echo " => Java: $(echo "$java" | sed "s;$HOME;~;")"
 echo " => Java Home: $(echo "$java_home" | sed "s;$HOME;~;")"
+echo " => Allow All Libraries: $allow_all_libraries"
 echo " => Allow Microphone: $allow_microphone"
 echo " => Allow Multiplayer: $allow_multiplayer"
 echo
@@ -77,10 +85,12 @@ echo "=============================================="
 exec sandbox-exec -f "$profile" \
   -Dminecraft="$minecraft" \
   -Dassets="$assets" \
+  -Dlibraries="$libraries" \
   -Djava="$java" \
   -Djava_home="$java_home" \
   -Dclasspath="$classpath" \
   -Dnative_dir="$native_dir" \
+  -Dallow_all_libraries="$allow_all_libraries" \
   -Dallow_microphone="$allow_microphone" \
   -Dallow_multiplayer="$allow_multiplayer" \
   -D_HOME="$HOME" \
